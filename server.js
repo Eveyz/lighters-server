488d78bcb03e2a1cf4db8a262fab70b3e2df1096
Ezio <nxz84@case.edu>
2020-06-01 11:07:53 -0400

    fix ssl location typo

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-const privateKey = fs.readFileSync(`${process.env.UBUNTU_SSL_LOCATION}/3999874_www.lightersenglish.com.key`, 'utf8');
-const certificate = fs.readFileSync(`${process.env.UBUNTU_SSL_LOCATION}/3999874_www.lightersenglish.com.pem`, 'utf8');
+const privateKey = fs.readFileSync(`${process.env.SSL_LOCATION}/3999874_www.lightersenglish.com.key`, 'utf8');
+const certificate = fs.readFileSync(`${process.env.SSL_LOCATION}/3999874_www.lightersenglish.com.pem`, 'utf8');
 const credentials = {key: privateKey, cert: certificate};
 
 const express = require("express");
